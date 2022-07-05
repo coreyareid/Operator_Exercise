7bf984aa6ada1e9fa24fe9169f9be353ed502ba7
coreyareid <coreyareid@bellsouth.net>
2022-07-05 11:38:29 -0500

    Completed the exercise

--- Operator_Exercise/Program.cs
+++ Operator_Exercise/Program.cs
@@ -1,3 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Exercise 1
 
+static  void Mathmatics()
+{
+    Console.WriteLine("Put in two numbers to get equations for math operators: " +
+        "addition, " +
+        "subtraction, " +
+        "multiplication, " +
+        "division, " +
+        "and the remainder.");
+    int x = Convert.ToInt32(Console.ReadLine());
+     int y = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine($"Your answer for addition is: "+"{0} + {1} = {2}", x, y, x + y);
+    Console.WriteLine($"Your answer for subtraction is: "+"{0} - {1} = {2}", x, y, x - y);
+    Console.WriteLine($"Your answer for multiplication is: " + "{0} * {1} = {2}", x, y, x * y);
+    Console.WriteLine($"Your answer for division is: " + "{0} / {1} = {2}", x, y, x / y);
+    Console.WriteLine($"Your answer for the remainder of: " + "{0} devided by {1} is {2}", x, y, x % y);
+}
+
+Mathmatics();
+
+//Exercise 2
+
+static void AreaOfCircle()
+{
+    Console.WriteLine("Input the radius of a circle to get the area.");
+    double radius = Convert.ToDouble(Console.ReadLine());
+    double area = Math.PI * radius * radius;
+    Console.WriteLine($"The area of a circle with a radius of: {radius}" + " is " + area);
+}
+
+AreaOfCircle();
